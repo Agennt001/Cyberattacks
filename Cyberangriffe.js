e7f2369c5324ee7a9ed1f0b1e1b0e6c195570632
Agennt0013 <92034022+Agennt0013@users.noreply.github.com>
2021-11-07 22:53:07 +0100

    Add files via upload

---
+++ Cyberangriffe.js
@@ -0,0 +1,7 @@
+function revealMessage() {
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
+
+function Message() {
+    document.getElementById("seenMessage").style.display= 'block';
+}
